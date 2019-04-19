6cfdc2a5497c640b97c91ece8745711a72d01d6c
aldo1400 <achoque1400@gmail.com>
2019-04-19 18:34:03 -0500

    before add api call

--- src/App.js
+++ src/App.js
@@ -2,27 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars',
+    desc:'A space movie'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: '36th Cahmber of Shaolin'
+  },
+];
+
 class App extends Component {
+
+state={
+  movies: []
+}
+  async componentDidMount() {
+    try{
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=63222e52f9045dc57b20e736f605be66&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch(e){
+      console.log(e);
+    }
+  }
+  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc}/>)}
       </div>
     );
   }
 }
+
 
 export default App;
